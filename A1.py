e6d45968f7a08c0e8cee60d64881ee67eec9c589
IsaqueCR4 <isaquecostarodrigues3@gmail.com>
2025-11-28 17:40:38 -0300

    Delete A1.py

--- A1.py
+++
@@ -1,3 +0,0 @@
-PROJETO-AVALIACÃO_REGIMENTAL-A1 ##Projeto de fundamentos em pytons — Avaliação A1
-
-Repositório destinado à Avaliação A1 da disciplina Técnicas de Desenvolvimento de Algoritmos, com foco em Estruturas Condicionais, Estruturas de Repetição, Listas e Dicionários, desenvolvido por meio de atividades práticas durante o curso.
